636c929a1a038923fc4032bfec3a90ece33af1d0
Ankush N <ankushn566@gmail.com>
2023-11-09 19:55:05 +0530

    Designed admin and user blog page, implemented pagination, wrote a method to filter the blog from the backend and integrated common blog_page.html page to both admin and user page.

--- BlogApplicationUsingSpringMVC/src/main/java/com/blogsculpture/exceptions/GlobalExceptionHandler.java
+++ BlogApplicationUsingSpringMVC/src/main/java/com/blogsculpture/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.blogsculpture.exceptions;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -7,13 +8,20 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoHandlerFoundException.class)
-    public String getAllExceptions() {
-        return "redirect:404";
-    }
-
-    @ExceptionHandler(NumberFormatException.class)
-    public String getNumberFormatterExceptin(NumberFormatException ne){
+    public String globalNoHandlerFoundExceptionsHandler(Model model, NoHandlerFoundException ne) {
+        model.addAttribute("message", ne.getMessage());
         return "error";
     }
 
+    @ExceptionHandler(NumberFormatException.class)
+    public String globalNumberFormatterExceptionHandler(Model model, NumberFormatException ne) {
+        model.addAttribute("message", ne.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(BlogExceptions.class)
+    public String globalBlogExceptionHandler(Model model, BlogExceptions be) {
+        model.addAttribute("message", be.getMessage());
+        return "error";
+    }
 }
